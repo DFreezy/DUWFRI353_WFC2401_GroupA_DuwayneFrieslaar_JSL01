379ecee6ba15a6a9fbd7d56fbf5a1e184f59e2ab
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-03-13 10:57:22 +0200

    Changed the comments, added trim() to the input variable
    Changed the comments, added trim() to the input variable to eliminate white spaces around the result

--- DUWFRI353_WFC2401_GroupA_DuwayneFrieslaar_JSL01/script.js
+++ DUWFRI353_WFC2401_GroupA_DuwayneFrieslaar_JSL01/script.js
@@ -1,9 +1,9 @@
 function validateSyntax() {
     //I am implementing a function which will respond to the onclick added to the validate button 
-    let input = document.getElementById('petInput').value; 
+    let input = document.getElementById('petInput').value.trim(); 
     //I am starting by fetching the input field from the DOM
     let regex = /^pet_[a-zA-Z0-9]+$/;
-    //Here I am adding the numerical values to a variable so I could split it up later
+    //Here I am adding the expression values to a variable so I could split it up later
     let petRegex = "pet_";
     let validInputRegex = new RegExp(`^${petRegex}[0-9]+[a-zA-Z]+$`)
     //Here I'm splitting the expression, and calling the variable I created a few lines up
@@ -13,9 +13,9 @@ function validateSyntax() {
 
     if (validInputRegex.test(input)) {
         //Here I am testing if the input matches the expression
-        result = 'Valid name'; // Set result to 'Valid' if input matches
+        result = 'Valid syntax'; // Set result to 'Valid' if input matches
     } else {
-        result = 'Invalid name'; // Set result to 'Invalid' if input does not match
+        result = 'Invalid syntax'; // Set result to 'Invalid' if input does not match
     }
             document.getElementById('result').innerText = result;
             /*Here I am showing the validation result to show if the input is valid by getting the result 
